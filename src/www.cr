dda028ea25a2dfa6e50cc383d258c11b59fe6b98
Tatsujin Chin <c910335@gmail.com>
2021-05-27 19:41:18 +0800

    remove setter

--- src/www.cr
+++ src/www.cr
@@ -3,12 +3,12 @@ require "http/client"
 class WWW
   VERSION = "0.1.0"
 
-  setter url = "https://www"
+  @url = "https://www"
 
   delegate inspect, to: to_s
 
   macro method_missing(name)
-    self.url = self.@url + ".{{name}}"
+    @url += ".{{name}}"
     self
   end
 
